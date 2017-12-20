19a3ef505c3d446413e6b9ab1269e6e84ac6f8b5
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-20 18:46:52 +0100

    added routes to pages gallery and rsvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/rsvp', to: 'pages#rsvp', as: 'rsvp'
+  get '/gallery', to: 'pages#gallery', as: 'gallery'
 end
