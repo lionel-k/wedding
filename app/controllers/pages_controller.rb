1c4433eb448e7197ead836ca7fa5482d22ae5a4c
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-20 18:47:27 +0100

    added empty pages rsvp and gallery

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :rsvp, :gallery]
 
   def home
   end
+
+  def rsvp
+  end
+
+  def gallery
+  end
+
 end
