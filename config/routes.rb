36eefd395b11469e1883e8c3310bef1fdc99e79f
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-21 14:52:20 +0100

    setup i18n for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  get '/rsvp', to: 'pages#rsvp', as: 'rsvp'
-  get '/gallery', to: 'pages#gallery', as: 'gallery'
+  scope '(:locale)', locale: /en|fr|hu|it/ do
+    root to: 'pages#home'
+    get '/rsvp', to: 'pages#rsvp', as: 'rsvp'
+    get '/gallery', to: 'pages#gallery', as: 'gallery'
+  end
 end
