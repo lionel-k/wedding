b7f4ba9f100f0bb3c96fd79d30e02e07ecf929af
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-22 16:40:38 +0100

    add lists of guests in .gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,24 @@
-GalleryPicture.destroy_all
+# GalleryPicture.destroy_all
 
 require "yaml"
+require "csv"
 
-gallery = YAML.load_file('db/gallery.yml')
-gallery.each do |picture|
-  name = picture["name"]
-  description = picture["description"]
-  GalleryPicture.create!(name: name, description: description)
+# gallery = YAML.load_file('db/gallery.yml')
+# gallery.each do |picture|
+#   name = picture["name"]
+#   description = picture["description"]
+#   GalleryPicture.create!(name: name, description: description)
+# end
+
+
+csv_options = { col_sep: ',', headers: :first_row }
+filepath    = 'db/guests.csv'
+
+CSV.foreach(filepath, csv_options) do |row|
+  Guest.create!(email: row['email'],
+    first_name: row['first_name'],
+    last_name: row['last_name'],
+    dear: row['dear'],
+    greeting: row['greeting'],
+    language: row['language'])
 end
