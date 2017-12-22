92e4a87eab7c3f6d081598a56a3eade1ba11b1ea
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-22 16:18:57 +0100

    added new table to handle the gallery pictures and the seeds

---
+++ db/migrate/20171222145107_create_gallery_pictures.rb
@@ -0,0 +1,10 @@
+class CreateGalleryPictures < ActiveRecord::Migration[5.1]
+  def change
+    create_table :gallery_pictures do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
