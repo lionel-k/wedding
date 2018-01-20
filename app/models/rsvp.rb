4d229d359a3c3622128908001e4ccbfdcb5d99ec
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-01-20 15:13:53 +0100

    handled errors of updating rsvp

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,5 +1,6 @@
 class Rsvp < ApplicationRecord
   belongs_to :guest
+  validates :guest, presence: true
   validates :is_attending, inclusion: { in: [ true, false ] }
   validates :needs_accomodation, inclusion: { in: [ true, false ] }
 end
