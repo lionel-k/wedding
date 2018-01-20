82d24190efdc0a674c68b704a506aa8508f5f13b
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-01-21 00:03:06 +0100

    add pre-defined values for validations in Rsvp model

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,5 +2,5 @@ class Rsvp < ApplicationRecord
   belongs_to :guest
   validates :guest, presence: true
   validates :is_attending, inclusion: { in: [ true, false ] }
-  validates :needs_accomodation, inclusion: { in: [ true, false ] }
+  validates :needs_accomodation, inclusion: { in: ["yes_hotel_incisa_single_room", "yes_hotel_incisa_double_room", "yes_agriturismo_loppiano", "no_thank_you"] }
 end
