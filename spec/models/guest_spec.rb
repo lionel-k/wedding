6d2d10e67a6ed45fa3483d3d5750e4dd58804620
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-10-08 14:11:00 +0200

    add config simplecov

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe Guest, type: :model do
     it { should validate_presence_of(:language) }
     it { should validate_presence_of(:greeting) }
   end
+
+  describe '#full_name' do
+    let(:guest) { FactoryBot.create(:guest) }
+    it 'render the full name of the guest' do
+      expect(guest.full_name).to eq("#{guest.first_name} #{guest.last_name}")
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,12 +24,8 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+
+ActiveRecord::Migration.maintain_test_schema!
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
